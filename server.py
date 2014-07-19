8aadb91445eeb0bb873ac90d8ada7c542c98eeea
Kai Si <kai.si@berkeley.edu>
2014-07-18 20:04:46 -0700

    shit i screwed something up:

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods = ["POST", "GET"])
-def hello():
+@app.route('/', methods = ["POST"])
+def submit_form():
     if request.method == 'POST':
         time = request.form['time']
         genre = request.form['genre']
@@ -12,4 +12,8 @@ def hello():
         # figure out what song to play
         #change html so that the song is played
 
+    
+
+@app.route('/')
+def at_first():
     return render_template('index.html')
